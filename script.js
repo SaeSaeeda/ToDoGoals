40f28089b3672b1646aa9b268f9acd6775caf9b1
Saeeda <ssa19@hi.is>
2023-12-26 23:12:23 +0000

    functionality is compelete

--- script.js
+++ script.js
@@ -1,49 +1,234 @@
-const inputBox = document.getElementById("hobbies-input-box")
-const listContainer = document.getElementById("hobbies-list")
+const inputBoxHobbies = document.getElementById("hobbies-input-box");
+const inputBoxWellness = document.getElementById("wellness-input-box");
+const inputBoxDate = document.getElementById("date-input-box");
+const inputBoxFinancial = document.getElementById("financial-input-box");
+const inputBoxTravel = document.getElementById("travel-input-box");
+const inputBoxselfcare = document.getElementById("selfcare-input-box");
 
-function addTask(inputId) {
-    const inputBox = document.getElementById(inputId);
-    const listContainerId = `${inputId}-list`;
-    const listContainer = document.getElementById(listContainerId);
+const wellnessList = document.getElementById("wellness-list");
+const financialList = document.getElementById("financial-list");
+const travelList = document.getElementById("travel-list");
+const selfcareList = document.getElementById("selfcare-list");
+const dateList = document.getElementById("date-list");
+const hobbiesList = document.getElementById("hobbies-list");
 
-    if (inputBox.value === '') {
-        alert('Please enter a task');
+function addTaskH() {
+    if (inputBoxHobbies.value === "") {
+        alert("Please enter a hobby");
     } else {
-        let li = document.createElement('li');
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement('span');
-        span.innerHTML = '\u00d7';
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        li.innerHTML = inputBoxHobbies.value;
+        span.innerHTML = "x";
         li.appendChild(span);
-    }
-    if (!listContainer) {
-        console.error(`List container with ID '${listContainerId}' not found.`);
-        return;
+        hobbiesList.appendChild(li);
     }
 
-    inputBox.value = '';
-    saveData();
+    inputBoxHobbies.value = "";
+    saveDataH();
 }
 
-listContainer.addEventListener('click', function (e) {
-    if (e.target.tagName === 'LI') {
-        e.target.classList.toggle('checked');
-        saveData();
+function addTaskD() {
+    if (inputBoxDate.value === "") {
+        alert("Please enter your date goals");
+    } else {
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        li.innerHTML = inputBoxDate.value;
+        span.innerHTML = "x";
+        li.appendChild(span);
+        dateList.appendChild(li);
     }
-    else if (e.target.tagName === 'SPAN') {
+
+    inputBoxDate.value = "";
+    saveDataD();
+}
+
+function addTaskW() {
+    if (inputBoxWellness.value === "") {
+        alert("Please enter your wellness activities");
+    } else {
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        li.innerHTML = inputBoxWellness.value;
+        span.innerHTML = "x";
+        li.appendChild(span);
+        wellnessList.appendChild(li);
+    }
+
+    inputBoxWellness.value = "";
+    saveDataW();
+}
+
+function addTaskF() {
+    if (inputBoxFinancial.value === "") {
+        alert("Please enter your financial goals");
+    } else {
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        li.innerHTML = inputBoxFinancial.value;
+        span.innerHTML = "x";
+        li.appendChild(span);
+        financialList.appendChild(li);
+    }
+
+    inputBoxFinancial.value = "";
+    saveDataF();
+}
+
+function addTaskT() {
+    if (inputBoxTravel.value === "") {
+        alert("Please enter your travel goals");
+    } else {
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        li.innerHTML = inputBoxTravel.value;
+        span.innerHTML = "x";
+        li.appendChild(span);
+        travelList.appendChild(li);
+    }
+
+    inputBoxTravel.value = "";
+    saveDataT();
+}
+
+function addTaskS() {
+    if (inputBoxselfcare.value === "") {
+        alert("Please enter your self-care goals");
+    } else {
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        li.innerHTML = inputBoxselfcare.value;
+        span.innerHTML = "x";
+        li.appendChild(span);
+        selfcareList.appendChild(li);
+    }
+
+    inputBoxselfcare.value = "";
+    saveDataS();
+}
+
+hobbiesList.addEventListener("click", function (e) {
+    handleListClick(e, hobbiesList, saveDataH);
+});
+
+dateList.addEventListener("click", function (e) {
+    handleListClick(e, dateList, saveDataD);
+});
+
+wellnessList.addEventListener("click", function (e) {
+    handleListClick(e, wellnessList, saveDataW);
+});
+
+financialList.addEventListener("click", function (e) {
+    handleListClick(e, financialList, saveDataF);
+});
+
+travelList.addEventListener("click", function (e) {
+    handleListClick(e, travelList, saveDataT);
+});
+
+selfcareList.addEventListener("click", function (e) {
+    handleListClick(e, selfcareList, saveDataS);
+});
+
+function handleListClick(e, list, saveDataFunction) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveDataFunction();
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        saveData();
+        saveDataFunction();
     }
-
-},false);
-
-
-function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function loadData() {
-    listContainer.innerHTML = localStorage.getItem("data");
+inputBoxHobbies.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addTaskH();
+    }
+});
+
+inputBoxDate.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addTaskD();
+    }
+});
+
+inputBoxWellness.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addTaskW();
+    }
+});
+
+inputBoxFinancial.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addTaskF();
+    }
+});
+
+inputBoxTravel.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addTaskT();
+    }
+});
+
+inputBoxselfcare.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addTaskS();
+    }
+});
+
+function saveDataH() {
+    localStorage.setItem("hobbiesdata", hobbiesList.innerHTML);
 }
 
-loadData();
+function saveDataD() {
+    localStorage.setItem("datedata", dateList.innerHTML);
+}
+
+function saveDataW() {
+    localStorage.setItem("wellnessdata", wellnessList.innerHTML);
+}
+
+function saveDataF() {
+    localStorage.setItem("financialdata", financialList.innerHTML);
+}
+
+function saveDataT() {
+    localStorage.setItem("traveldata", travelList.innerHTML);
+}
+
+function saveDataS() {
+    localStorage.setItem("selfdata", selfcareList.innerHTML);
+}
+
+function loadDataH() {
+    hobbiesList.innerHTML = localStorage.getItem("hobbiesdata");
+}
+
+function loadDataD() {
+    dateList.innerHTML = localStorage.getItem("datedata");
+}
+
+function loadDataW() {
+    wellnessList.innerHTML = localStorage.getItem("wellnessdata");
+}
+
+function loadDataF() {
+    financialList.innerHTML = localStorage.getItem("financialdata");
+}
+
+function loadDataT() {
+    travelList.innerHTML = localStorage.getItem("traveldata");
+}
+
+function loadDataS() {
+    selfcareList.innerHTML = localStorage.getItem("selfdata");
+}
+
+loadDataH();
+loadDataD();
+loadDataW();
+loadDataF();
+loadDataT();
+loadDataS();
